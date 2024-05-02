b0e4d3eb08a3f411629ffb69ab95f08467b950de
LoPhong Corporation Official <165672787+LoPhong-Corporation-Official@users.noreply.github.com>
2024-05-02 18:34:55 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            IPAddress ipAddress = IPAddress.Parse("192.168.1.4");
+            int port = 50795;
+
+            TcpClient clientSocket = new TcpClient();
+            clientSocket.Connect(ipAddress, port);
+            NetworkStream networkStream = clientSocket.GetStream();
+
+            while (true)
+            {
+                Console.WriteLine("Enter data to send to Server:");
+                string message = Console.ReadLine();
+
+                byte[] sendBytes = Encoding.ASCII.GetBytes(message + "$");
+                networkStream.Write(sendBytes, 0, sendBytes.Length);
+                networkStream.Flush();
+
+                byte[] bytesFrom = new byte[clientSocket.ReceiveBufferSize];
+                int bytesRead = networkStream.Read(bytesFrom, 0, clientSocket.ReceiveBufferSize);
+                string dataFromServer = Encoding.ASCII.GetString(bytesFrom, 0, bytesRead);
+                Console.WriteLine("Server response:" + dataFromServer);
+            }
+
+            clientSocket.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
